13f539ea90c84d04adf3ab3cdf79d0176c5360a0
Divyanshu <117548669+Divyanshu6797@users.noreply.github.com>
2024-05-08 05:38:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from email.mime.text import MIMEText
 app = Flask(__name__)
 CORS(app)
 
+api = Api(app)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 def sendmail(data):
     work = data.get('work')
     room_no = data.get('room_no')
